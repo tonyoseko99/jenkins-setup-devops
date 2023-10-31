9fd04130be1e56dba865bad93714f72366637828
Tonny Oseko <tonnyoseko0@gmail.com>
2023-10-31 14:38:45 +0300

    Add a division test Tested the division of two numbers

---
+++ divide.py
@@ -0,0 +1,5 @@
+def divide(num1, num2):
+    return ( num1 / num2)
+
+result = divide(1,2)
+print(result)
---
+++ test_divide.py
@@ -0,0 +1,13 @@
+import unittest
+from divide import divide
+
+class TestDivide(unittest.TestCase):
+    
+    def test_divide(self):
+        self.assertEqual(divide(4,2), 2, "Should be 2")
+        self.assertEqual(divide(0,5), 0, "Should be 0")
+        self.assertEqual(divide(-10,2), -5, "Should be -5")
+        self.assertRaises(ZeroDivisionError, divide, 1, 0)
+
+if __name__ == '__main__':
+    unittest.main()
